163be86f8161ca74a6483cec62bbdcddf1a4b15f
xiaozhi止水 <877657682@qq.com>
2019-02-25 14:57:24 +0800

    Initial commit

---
+++ src/main/java/com/ts/provider/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ts.provider.mapper;
+
+import com.ts.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
